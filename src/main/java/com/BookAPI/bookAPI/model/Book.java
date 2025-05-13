62941a16f01be27984a4b8759ad33edd1dae55ef
Marinescu Monica <marinescumonica97@yahoo.com>
2025-05-13 19:45:57 +0300

    Added session 20 - REST APIs

---
+++ src/main/java/com/BookAPI/bookAPI/model/Book.java
@@ -0,0 +1,15 @@
+package com.BookAPI.bookAPI.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+    private int year;
+}
